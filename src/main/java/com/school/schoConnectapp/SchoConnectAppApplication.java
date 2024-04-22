509fa9ccf6f17874f4a8d968ea166d0d30b046a8
DhyeyaPatel <pateldhyeya7@gmail.com>
2024-04-23 00:11:09 +0530

    Added initial folder structure

---
+++ src/main/java/com/school/schoConnectapp/SchoConnectAppApplication.java
@@ -0,0 +1,13 @@
+package com.school.schoConnectapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoConnectAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoConnectAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/school/schoConnectapp/SchoConnectAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.schoConnectapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoConnectAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
